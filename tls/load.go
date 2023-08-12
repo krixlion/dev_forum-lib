9b9bed68045314c75f39a856b61face751083868
krixlion <konstanty.cyran.k@gmail.com>
2023-08-12 18:47:14 +0200

    Add tls cert loading

--- tls/load.go
+++ tls/load.go
@@ -2,21 +2,42 @@ package tls
 
 import (
 	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"os"
 
 	"google.golang.org/grpc/credentials"
 )
 
-// LoadCredentials loads server's certificate and private key from provided filepaths.
-func LoadCredentials(certPath, keyPath string) (credentials.TransportCredentials, error) {
+// LoadServerCredentials loads server's certificate and private key from provided filepaths.
+func LoadServerCredentials(certPath, keyPath string) (credentials.TransportCredentials, error) {
 	serverCert, err := tls.LoadX509KeyPair(certPath, keyPath)
 	if err != nil {
 		return nil, err
 	}
 
-	// Create the credentials and return it
 	config := &tls.Config{
 		Certificates: []tls.Certificate{serverCert},
-		ClientAuth:   tls.NoClientCert,
+		ClientAuth:   tls.VerifyClientCertIfGiven,
+	}
+
+	return credentials.NewTLS(config), nil
+}
+
+func LoadCA(path string) (credentials.TransportCredentials, error) {
+	pemServerCA, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	certPool := x509.NewCertPool()
+
+	if !certPool.AppendCertsFromPEM(pemServerCA) {
+		return nil, fmt.Errorf("failed to add server CA's certificate")
+	}
+
+	config := &tls.Config{
+		RootCAs: certPool,
 	}
 
 	return credentials.NewTLS(config), nil
