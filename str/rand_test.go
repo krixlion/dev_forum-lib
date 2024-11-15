f25a7093152d6420d924b01b3bb964db646d29ae
Konstanty Cyran <96084033+krixlion@users.noreply.github.com>
2024-11-15 15:32:17 +0100

    test(str): transform table driven test into simple unit test

--- str/rand_test.go
+++ str/rand_test.go
@@ -3,33 +3,16 @@ package str
 import "testing"
 
 func Test_RandomAlphaString(t *testing.T) {
-	type args struct {
-		length int
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    string
-		wantErr bool
-	}{
-		{
-			name: "Test if returned string has correct length",
-			args: args{
-				length: 5,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := RandomAlphaString(tt.args.length)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("randomAlphaString() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
+	t.Run("Test if returned string has correct length", func(t *testing.T) {
+		length := 5
+		got, err := RandomAlphaString(length)
+		if err != nil {
+			t.Errorf("randomAlphaString() error = %v", err)
+			return
+		}
 
-			if len(got) != tt.args.length {
-				t.Errorf("randomAlphaString() invalid length: got = %v expected length %v", got, tt.args.length)
-			}
-		})
-	}
+		if len(got) != length {
+			t.Errorf("randomAlphaString() invalid length: got = %v expected length %v", got, length)
+		}
+	})
 }
