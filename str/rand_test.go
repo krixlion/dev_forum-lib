cb5ecd676aaee1cd57723f778c7e121345c1f266
Konstanty Cyran <96084033+krixlion@users.noreply.github.com>
2024-11-15 15:38:11 +0100

    test(str): add test case for subsequent calls returning same results

--- str/rand_test.go
+++ str/rand_test.go
@@ -7,12 +7,32 @@ func Test_RandomAlphaString(t *testing.T) {
 		length := 5
 		got, err := RandomAlphaString(length)
 		if err != nil {
-			t.Errorf("randomAlphaString() error = %v", err)
+			t.Errorf("RandomAlphaString() error = %v", err)
 			return
 		}
 
 		if len(got) != length {
-			t.Errorf("randomAlphaString() invalid length: got = %v expected length %v", got, length)
+			t.Errorf("RandomAlphaString() invalid length: got = %v expected length %v", got, length)
+		}
+	})
+
+	t.Run("Test if two equal subsequent calls return different results", func(t *testing.T) {
+		length := 5
+
+		first, err := RandomAlphaString(length)
+		if err != nil {
+			t.Errorf("RandomAlphaString() error = %v", err)
+			return
+		}
+
+		second, err := RandomAlphaString(length)
+		if err != nil {
+			t.Errorf("RandomAlphaString() error = %v", err)
+			return
+		}
+
+		if first == second {
+			t.Errorf("RandomAlphaString(): returned the same string twice:\n first = %v\n second = %v\n", first, second)
 		}
 	})
 }
